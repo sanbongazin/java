16853d6fe5ded72d89cb15462bf0d72a2db9cef0
sanbongazin <sanbongazin@gmail.com>
2018-07-05 16:53:44 +0900

    最新ファイル更新分

---
+++ MyP2dTest/src/MyP2d.java
@@ -0,0 +1,15 @@
+
+public class MyP2d {
+	private double x;
+	private double y;
+
+	public MyP2d(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void showPoint() {
+		System.out.println("(" + x + "," + y +")");
+	}
+
+}
---
+++ MyP2dTest/src/MyP2dTest.java
@@ -0,0 +1,9 @@
+
+public class MyP2dTest {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
